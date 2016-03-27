6daa02259511b36a50c53f0753c79d65bde29d53
kbrawley95 <kieran.brawley95@gmail.com>
2016-03-27 05:07:41 +0100

    Layout Adapter (Fix)
    Previous issue:
    -Layouts showed multiple copies of the same result.
    
    This problem has been amended.

--- TrafficRSSProject(XMLPullParser)/app/src/main/java/com/example/kieran/trafficrssprojectxmlpullparser/RSSAdapter.java
+++ TrafficRSSProject(XMLPullParser)/app/src/main/java/com/example/kieran/trafficrssprojectxmlpullparser/RSSAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Filterable;
 import android.widget.Filter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,26 +31,27 @@ public class RSSAdapter extends ArrayAdapter<RSSItem>{
     @Override
     public View getView(int pos, View convertView, ViewGroup parent){
 
-        //Get the data item for this position
-        RSSItem rssItem = getItem(pos);
+        RelativeLayout row=(RelativeLayout)convertView;
+
         //Check if an existing view is being reused, otherwise inflate the view
-        if(convertView==null){
-            convertView=LayoutInflater.from(getContext()).inflate(R.layout.row_site, parent,false);
-
-            //Lookup view for data population
-            TextView titleText=(TextView)convertView.findViewById(R.id.homeText);
-            TextView descriptionText=(TextView)convertView.findViewById(R.id.descriptionText);
-            TextView linkText=(TextView)convertView.findViewById(R.id.linkText);
-            TextView pubDateText=(TextView)convertView.findViewById(R.id.pubDateText);
-            //Populate the data in the template view using the data object
-            titleText.setText(rssItem.getTitle());
-            descriptionText.setText(rssItem.getDescription());
-            linkText.setText(rssItem.getLink());
-            pubDateText.setText(rssItem.getPubDate());
-
+        if(row==null) {
+            LayoutInflater inflater=(LayoutInflater)parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row=(RelativeLayout)inflater.inflate(R.layout.row_site, null);
         }
 
-        return convertView;
+            //Lookup view for data population
+            TextView titleText=(TextView)row.findViewById(R.id.homeText);
+            TextView descriptionText=(TextView)row.findViewById(R.id.descriptionText);
+            TextView linkText=(TextView)row.findViewById(R.id.linkText);
+            TextView pubDateText=(TextView)row.findViewById(R.id.pubDateText);
+
+            //Populate the data in the template view using the data object
+            titleText.setText(getItem(pos).getTitle());
+            descriptionText.setText(getItem(pos).getDescription());
+            linkText.setText(getItem(pos).getLink());
+            pubDateText.setText(getItem(pos).getPubDate());
+
+        return row;
     }
 }
 
